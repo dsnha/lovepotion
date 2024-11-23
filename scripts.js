ae8c1aaeb449e9511d2e26cc83d42370c0ebca33
dsnha <dsnha.06@gmail.com>
2024-11-23 22:03:17 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let currentLevel = 0;
 function movePotion() {
   const liquid = document.querySelector('.liquid');
   currentLevel += 10; // Increase the potion's height by 10%
-  
+
   // Restrict the potion from overflowing the bottle
   if (currentLevel > 100) {
     currentLevel = 100;
