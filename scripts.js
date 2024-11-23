466f052e2246594bbbfc6895e7c050437f2c2679
dsnha <dsnha.06@gmail.com>
2024-11-23 21:41:36 +0800

    Create script.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let currentLevel = 0;
+
+function movePotion() {
+  const liquid = document.querySelector('.liquid');
+  currentLevel += 10; // Increase the potion's height by 10%
+  
+  // Restrict the potion from overflowing the bottle
+  if (currentLevel > 100) {
+    currentLevel = 100;
+  }
+
+  // Move the potion upward by adjusting its transform property
+  liquid.style.transform = `translateY(-${currentLevel}%)`;
+}
